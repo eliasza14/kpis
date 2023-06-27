844ddb71664ca0e0c2016afe064d49184fe92570
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-27 17:19:09 +0300

    message

--- app.py
+++ app.py
@@ -30,12 +30,20 @@ def main():
     selected_option7 = st.sidebar.button("button7")
 
 
-    selected_item = st.sidebar.selectbox("", ["ad", "e", "pinkas"])
+    # selected_item = st.sidebar.selectbox("", ["ad", "e", "pinkas"])
     
     if selected_option1:
-        display_ad_submenu()
+        ad_button1(id)
     elif selected_option2:
-        display_e_submenu()
+        ad_button2(id)
+    elif selected_option3:
+        ad_button3(id)
+    elif selected_option4:
+        ad_button4(id)
+    elif selected_option5:
+        ad_button5(id)
+    elif selected_option6:
+        ad_button6(id)
     elif selected_option7:
         display_pinkas_submenu(id)
 
@@ -44,37 +52,54 @@ def main():
 
 
 
-    if selected_item == "ad":
-        display_ad_submenu()
-    elif selected_item == "e":
-        display_e_submenu()
-    elif selected_item == "pinkas":
-        display_pinkas_submenu(id)
+    # if selected_item == "ad":
+    #     display_ad_submenu()
+    # elif selected_item == "e":
+    #     display_e_submenu()
+    # elif selected_item == "pinkas":
+    #     display_pinkas_submenu(id)
 
-def display_ad_submenu():
-    st.subheader("ad Submenu")
-    ad_options = ["2", "3"]
-    selected_option = st.selectbox("Select an option", ad_options)
+# def display_ad_submenu():
+#     st.subheader("ad Submenu")
+#     ad_options = ["2", "3"]
+#     selected_option = st.selectbox("Select an option", ad_options)
     
-    if selected_option == "2":
-        st.write("Content for Option 2 in ad submenu")
-        # Add content for Option 2 in ad submenu here
-    elif selected_option == "3":
-        st.write("Content for Option 3 in ad submenu")
-        # Add content for Option 3 in ad submenu here
+#     if selected_option == "2":
+#         st.write("Content for Option 2 in ad submenu")
+#         # Add content for Option 2 in ad submenu here
+#     elif selected_option == "3":
+#         st.write("Content for Option 3 in ad submenu")
+#         # Add content for Option 3 in ad submenu here
 
-def display_e_submenu():
-    st.subheader("e Submenu")
-    e_options = ["2", "5"]
-    selected_option = st.selectbox("Select an option", e_options)
+# def display_e_submenu():
+#     st.subheader("e Submenu")
+#     e_options = ["2", "5"]
+#     selected_option = st.selectbox("Select an option", e_options)
     
-    if selected_option == "2":
-        st.write("Content for Option 2 in e submenu")
-        # Add content for Option 2 in e submenu here
-    elif selected_option == "5":
-        st.write("Content for Option 5 in e submenu")
-        # Add content for Option 5 in e submenu here
-
+#     if selected_option == "2":
+#         st.write("Content for Option 2 in e submenu")
+#         # Add content for Option 2 in e submenu here
+#     elif selected_option == "5":
+#         st.write("Content for Option 5 in e submenu")
+#         # Add content for Option 5 in e submenu here
+def ad_button1(id):
+    st.subheader("button1 Submenu")
+    st.write("Content of button1")
+def ad_button2(id):
+    st.subheader("button2 Submenu")
+    st.write("Content of button2")
+def ad_button3(id):
+    st.subheader("button3 Submenu")
+    st.write("Content of button3")
+def ad_button4(id):
+    st.subheader("button4 Submenu")
+    st.write("Content of button4")
+def ad_button5(id):
+    st.subheader("button5 Submenu")
+    st.write("Content of button5")
+def ad_button6(id):
+    st.subheader("button6 Submenu")
+    st.write("Content of button6")
 def display_pinkas_submenu(id):
     st.subheader("pinkas Submenu")
     st.write("Content for pinkas submenu")
