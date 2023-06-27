7f921e369cea8a8ab8e669cb80d50b7d94c4c95b
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-27 15:41:54 +0300

    message

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import streamlit as st
 def main():
     st.sidebar.title("Menu")
     
-    ad_expander = st.sidebar.beta_expander("ad")
+    ad_expander = st.sidebar.expander("ad")
     with ad_expander:
         st.write("-> 2")
         st.write("-> 3")
 
-    e_expander = st.sidebar.beta_expander("e")
+    e_expander = st.sidebar.expander("e")
     with e_expander:
         st.write("-> 2")
         st.write("-> 5")
