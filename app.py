7445a2025774600f198756e8ae5e574372da614e
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-28 10:54:38 +0300

    message

--- app.py
+++ app.py
@@ -102,9 +102,9 @@ def ad_button1(id):
 
     st.write(df)
     koispe1df=df[df['id']==id]
-    st.write(koispe1df['profile.meli_a'])
-    st.write(koispe1df['profile.meli_b'])
-
+    
+    st.write(koispe1df['profile.meli_a']+koispe1df['profile.meli_b']+koispe1df['profile.meli_c'])
+    
     st.metric(label="Gas price", value=4, delta=-0.5,delta_color="inverse")
 
     st.metric(label="Active developers", value=123, delta=123, delta_color="off")
