b2cf3bd98520b5f229d48c9c504073c81ca3e078
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-28 18:07:37 +0300

    message

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def ad_button1(id,kpdf):
                 ))
 
             # Update the layout
-            fig.update_layout(barmode='stack', title='100% Stacked Bar Plot', xaxis_title='Year'yaxis_title='Percentage')
+            fig.update_layout(barmode='stack', title='100% Stacked Bar Plot', xaxis_title='Year',yaxis_title='Percentage')
             # Show the plot
 
             st.plotly_chart(fig)
