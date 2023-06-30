f72e52e82edeb1b8a8ffdd75bdc8d022c282955f
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-30 16:44:19 +0300

    message

--- app.py
+++ app.py
@@ -426,6 +426,53 @@ def ad_button3(id,kpdf):
 
         with col2:
             st.write('Δ13')
+            st.write('Δ13')
+            categories=kpdf['year'].tolist()
+            st.write(categories)
+            # Sample data
+            # categories = ['Category A', 'Category B', 'Category C', 'Category D']
+            values =kpdf['D13'].tolist()
+            st.write(values)
+
+            # Calculate percentage change
+            percentage_change = [(values[i] - values[i-1]) / values[i-1] * 100 for i in range(1, len(values))]
+
+            # Create the bar trace
+            bar_trace = go.Bar(x=categories, y=values, name='Values')
+
+            # Create the line trace
+            line_trace = go.Scatter(x=categories[1:], y=percentage_change, name='Percentage Change', mode='lines+markers', yaxis='y2')
+
+            # Create the layout with two y-axes
+            layout = go.Layout(
+                title='Bar Chart with Percentage Change',
+                yaxis=dict(title='Values', rangemode='nonnegative'),
+                yaxis2=dict(title='Percentage Change', overlaying='y', side='right', showgrid=False)
+            )
+
+            # Create the figure
+            fig = go.Figure(data=[bar_trace, line_trace], layout=layout)
+
+            # Add labels to the bars
+            for i in range(len(categories)):
+                fig.add_annotation(
+                    x=categories[i], y=values[i],
+                    text=str(values[i]),
+                    showarrow=False,
+                    font=dict(color='black', size=12),
+                    xanchor='center', yanchor='bottom'
+                )
+
+            # Add labels to the percentage change
+            for i in range(len(percentage_change)):
+                fig.add_annotation(
+                    x=categories[i+1], y=percentage_change[i],
+                    text=f"{percentage_change[i]:.2f}%",
+                    showarrow=False,
+                    font=dict(color='red', size=12),
+                    xanchor='center', yanchor='bottom'
+                )
+            st.plotly_chart(fig)
         
           
 
