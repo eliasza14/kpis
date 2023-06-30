34ec54aea267237791d39d7fcdcf2cd3254b4bf5
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-30 15:04:00 +0300

    message

--- app.py
+++ app.py
@@ -165,16 +165,16 @@ def ad_button2(id,kpdf):
     with st.container():
         col1, col2,col3 = st.columns(3)
         with col1:
-            st.write('D3')
+            st.write('Δ3-Εργαζόμενοι Γενικού Πληθυσμού')
             st.write(kpdf['D3'][kpdf['year']==str(year_filter)])
 
         with col2:
-            st.write('D5')
+            st.write('Δ5-Εργαζόμενοι ΛΥΨΥ')
             st.write(kpdf['D5'][kpdf['year']==str(year_filter)])
 
           
         with col3:
-            st.write('D7')
+            st.write('D7-Εργαζόμενοι ΕΚΟ')
             st.write(kpdf['D7'][kpdf['year']==str(year_filter)])
 
 
