e14fd34a197a957eb99d79bb3abd55a903ba6a02
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-06-30 14:23:43 +0300

    message

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def ad_button1(id,kpdf):
         col1, col2,col3 = st.columns(3)
         with col1:
             st.write('Col1 show D1')
-            st.write(kpdf['D1'][kpdf['year']==str(year_filter)])
+            st.write(kpdf['D1'][kpdf['year']==str(year_filter)].value)
             # st.metric(label="Συνολο Μελών "+str(kpdf['D1'][kpdf['year']==str(year_filter)][0]), value=int(kpdf['D1'][kpdf['year']==str(year_filter)][0]), delta=-0.5,delta_color="inverse")
 
         with col2:
