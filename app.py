89a408a4c072a4da8927ce37d36c0c6f28fa8f0e
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-03 14:19:36 +0300

    message

--- app.py
+++ app.py
@@ -570,7 +570,7 @@ def ad_button4(id,kpdf):
                                 textinfo='none',
                                 marker_colors=['rgb(113,209,145)','rgb(240,240,240)'],
                                 ))
-            fig.update_layout(annotations=[dict(text=str(val)+"%", x=0.2, y=0.5, font_size=20, showarrow=False)])
+            fig.update_layout(annotations=[dict(text=str(val)+"%",  font_size=20, showarrow=False)])
             st.plotly_chart(fig)
 
     
