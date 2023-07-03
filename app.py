d9f2cc3b0329ae7b7aca154957f95b17954e0987
A-PALIOS <paliosadonis6@gmail.com>
2023-07-03 17:00:50 +0300

    message

--- app.py
+++ app.py
@@ -171,23 +171,23 @@ def ad_button2(id,kpdf):
     with st.container():
         col1, col2,col3 = st.columns(3)
         with col1:
-            st.write('Δ3-Εργαζόμενοι Γενικού Πληθυσμού')
-            text=kpdf['D3'][kpdf['year']==str(year_filter)].iloc[0]
-            st.write(text)
+            
+            text=str(kpdf['D3'][kpdf['year']==str(year_filter)].iloc[0])
+            st.write('Δ3-Εργαζόμενοι Γενικού Πληθυσμού'+text)
             #st.write(kpdf['D3'][kpdf['year']==str(year_filter)])
 
         with col2:
-            st.write('Δ5-Εργαζόμενοι ΛΥΨΥ')
-            text=kpdf['D5'][kpdf['year']==str(year_filter)].iloc[0]
-            st.write(text)
-            #st.write(kpdf['D5'][kpdf['year']==str(year_filter)])
             
+            text=kpdf['D5'][kpdf['year']==str(year_filter)].iloc[0]
+            st.write('Δ5-Εργαζόμενοι ΛΥΨΥ'+text)
+            #st.write(kpdf['D5'][kpdf['year']==str(year_filter)])
+
 
           
         with col3:
-            st.write('D7-Εργαζόμενοι ΕΚΟ')
+            #st.write('D7-Εργαζόμενοι ΕΚΟ')
             text=kpdf['D7'][kpdf['year']==str(year_filter)].iloc[0]
-            st.write(text)
+            st.write('D7-Εργαζόμενοι ΕΚΟ'+text)
             #st.write(kpdf['D7'][kpdf['year']==str(year_filter)])
 
     with st.container():
