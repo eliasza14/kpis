9f0ce506fdd8aa86c7e77f6252bcdcb2dfb2dd6e
A-PALIOS <paliosadonis6@gmail.com>
2023-07-04 14:49:44 +0300

    message

--- app.py
+++ app.py
@@ -1121,27 +1121,13 @@ def e_button7(id,kpdf):
                 js_code2 = file.read()
             html(f"""
             
-                        <style>
-                        #myProgress {
-                            width: 100%;
-                            background-color: #ddd;
-                        }
-
-                        #myBar {
-                            width: 10%;
-                            height: 30px;
-                            background-color: #04AA6D;
-                            text-align: center;
-                            line-height: 30px;
-                            color: white;
-                        }
-                        </style>
+                        
                         <body>
 
                         <h1>JavaScript Progress Bar</h1>
 
-                        <div id="myProgress">
-                        <div id="myBar">10%</div>
+                        <div id="myProgress" style="width: 100%; background-color: #ddd;">
+                        <div id="myBar" style="width: 10%; height: 30px; background-color: #04AA6D; text-align: center; line-height: 30px; color: white;">10%</div>
                         </div>
 
                         <br>
