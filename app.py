92dd97ca22e70136742a86fe1a46235980f8df82
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-04 17:59:07 +0300

    message

--- app.py
+++ app.py
@@ -826,7 +826,7 @@ def ad_button4(id,kpdf,js_code):
 
 
 
-def e_button5(id,kpdf,js_code):
+def e_button5(id,kpdf):
     st.subheader("button5 Submenu")
     st.write("Content of button5")
     year_filter = st.selectbox("Έτος", kpdf['year'].tolist())
@@ -835,17 +835,7 @@ def e_button5(id,kpdf,js_code):
         #st.write(first_alias_value)
         #st.markdown(text)
     st.title(text)
-    st.markdown("<h3 style='text-align: center; color: grey;'>Κυκλοι Εργασιών</h3>", unsafe_allow_html=True)
-
-    html(
-                f"""<body style="display: flex;flex-wrap: nowrap;align-content: center;justify-content: center;">
-                <div id="counter" style="text-align: center; font-weight: bold; font-size: 60px; background-color: #f1f1f1; width: 130px; height: 130px; border-radius: 50%; display: flex; align-items: center; justify-content: center;"></div>
-                <script type="text/javascript">
-                {js_code}
-                animateCounter("counter", 0, """+str(val2)+""", 1000);  // Increase from 0 to 100 in 1 second
-                </script></body>
-                """
-            )
+   
     with st.container():
         col1, col2,col3 = st.columns(3)
         with col1:
