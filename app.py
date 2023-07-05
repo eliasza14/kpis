eab0a0ebfd3c7a901cb39282fa3817632a5bdc5c
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-05 11:18:53 +0300

    message

--- app.py
+++ app.py
@@ -380,7 +380,7 @@ def ad_button2(id,kpdf,js_code):
                         textposition='inside'
                     ))
                 # Update the layout
-                fig.update_layout(barmode='stack', xaxis_title='Έτος',yaxis_title='% επι του Συνόλου')
+                fig.update_layout(barmode='stack', xaxis_title='Έτος',yaxis_title='% επι του Συνόλου',height=300, width=400)
 
                 # Show the plot
                 st.plotly_chart(fig, use_container_width=True)
