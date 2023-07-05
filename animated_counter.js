639270937aa190d5bfa66559eaf0db68934794f2
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-05 15:12:03 +0300

    message

--- animated_counter.js
+++ animated_counter.js
@@ -29,3 +29,19 @@ function animateCounter2(elementId, startValue, endValue, duration) {
         }
     }, stepTime);
 }
+
+function animateCounter3(elementId, startValue, endValue, duration) {
+    let current = startValue;
+    const range = endValue - startValue;
+    const increment = endValue > startValue ? 0.1 : -0.1;
+    const stepTime = Math.abs(Math.floor(duration / Math.abs(range)));
+    const decimalPlaces = (endValue.toString().split('.')[1] || []).length;
+    const element = document.getElementById(elementId);
+    const timer = setInterval(() => {
+        current += increment;
+        element.textContent = '€' + current.toFixed(decimalPlaces);
+        if ((increment > 0 && current >= endValue) || (increment < 0 && current <= endValue)) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
--- app.py
+++ app.py
@@ -873,7 +873,7 @@ def e_button5(id,kpdf,js_code):
         <div id="counter" style="text-align: center; font-weight: bold; font-size: 60px; background-color: #f1f1f1; width: 130px; height: 130px; border-radius: 50%; display: flex; align-items: center; justify-content: center;"></div>
         <script type="text/javascript">
         {js_code}
-        animateCounter2("counter", 0, """+str(val2)+""", 1000);  // Increase from 0 to 100 in 1 second
+        animateCounter3("counter", 0, """+str(val2)+""", 1000);  // Increase from 0 to 100 in 1 second
         </script></body>
         """
     )
