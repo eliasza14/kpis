800b942258d8c8dd98a3c76b59b1e3c771c213ea
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-06 15:45:27 +0300

    message

--- app.py
+++ app.py
@@ -143,9 +143,8 @@ def main():
     # merged=pd.merge(dfs,on=['koispe_id','year'])
 
     st.write(merged)
-    
-    # kdata=merged[merged['koispe_id']==int(id)]
-    kdata=merged[merged['id']==int(id)]
+    merged.rename(columns={'id': 'koispe_id'}, inplace=True)
+    kdata=merged[merged['koispe_id']==int(id)]
 
 
     # kdata.drop(columns=['id_x', 'id_y','id'],inplace=True)
