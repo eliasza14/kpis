ca3e619d876723a3e1c5cfee613577703138d962
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-07 12:01:25 +0300

    message

--- app.py
+++ app.py
@@ -566,37 +566,39 @@ def ad_button4(id,kpdf,js_code):
             st.markdown("<h3 style='text-align: center; color: grey;'>Ετήσιες Μονάδες Εργασίας ΕΚΟ % επί του Συνόλου</h3>", unsafe_allow_html=True)
 
             val2=float(kpdf['D23'][kpdf['year']==str(year_filter)].iloc[0])
-            layout = go.Layout(
-            yaxis=dict(title='Values', rangemode='nonnegative'),
-            yaxis2=dict(title='Ποσοστιαία μεταβολή', overlaying='y', side='right', showgrid=False),
-            height=400,  # Set the height of the chart
-            width=400,  # Set the width of the chart
-            legend=dict(
-                orientation='h',
-                yanchor='top',
-                y=1.1,
-                xanchor='center',
-                x=0.5
-            ),
-            margin=dict(l=0, r=0, t=30, b=0, autoexpand=True)  # Set the margin to auto
-            )
-            fig = go.Figure(layout=layout)
-            fig.add_trace(go.Pie(
-                labels=['(%) Μ.Ε. ΕΚΟ επι του συνόλου', ' '],
-                values=[val2,100-val2],
-                hole=0.85,
-                textinfo='none',
-                marker_colors=['rgb(113,209,145)','rgb(240,240,240)'],
-            ))
-            fig.update_layout(annotations=[dict(text=str(val2) + "%", font_size=40, showarrow=False)])
-            fig.update_layout(showlegend=True)  # Show the legend
-            fig.update_layout(legend=dict(
-                orientation='h',
-                yanchor='top',
-                y=1.1,
-                xanchor='center',
-                x=0.5
-            ))
+            # layout = go.Layout(
+            # yaxis=dict(title='Values', rangemode='nonnegative'),
+            # yaxis2=dict(title='Ποσοστιαία μεταβολή', overlaying='y', side='right', showgrid=False),
+            # height=400,  # Set the height of the chart
+            # width=400,  # Set the width of the chart
+            # legend=dict(
+            #     orientation='h',
+            #     yanchor='top',
+            #     y=1.1,
+            #     xanchor='center',
+            #     x=0.5
+            # ),
+            # margin=dict(l=0, r=0, t=30, b=0, autoexpand=True)  # Set the margin to auto
+            # )
+            # fig = go.Figure(layout=layout)
+            # fig.add_trace(go.Pie(
+            #     labels=['(%) Μ.Ε. ΕΚΟ επι του συνόλου', ' '],
+            #     values=[val2,100-val2],
+            #     hole=0.85,
+            #     textinfo='none',
+            #     marker_colors=['rgb(113,209,145)','rgb(240,240,240)'],
+            # ))
+            # fig.update_layout(annotations=[dict(text=str(val2) + "%", font_size=40, showarrow=False)])
+            # fig.update_layout(showlegend=True)  # Show the legend
+            # fig.update_layout(legend=dict(
+            #     orientation='h',
+            #     yanchor='top',
+            #     y=1.1,
+            #     xanchor='center',
+            #     x=0.5
+            # ))
+
+            fig=donut_pct_Chart(val2,'rgb(113,209,145)','rgb(240,240,240)',['(%) Μ.Ε. ΕΚΟ επι του συνόλου', ' '])
             st.plotly_chart(fig,use_container_width=True)
 
     
