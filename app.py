dadf8e98792f46418ff9d1a458880426811b73c7
A-PALIOS <paliosadonis6@gmail.com>
2023-07-10 16:19:53 +0300

    message

--- app.py
+++ app.py
@@ -248,7 +248,7 @@ def ad_button1(id,kpdf,js_code):
                 f"""<body style="display: flex;flex-wrap: nowrap;align-content: center;justify-content: center;">
                 <div style="background: rgb(34,193,195);background: linear-gradient(45deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%); width:200px;">
                     <div>💀</div>
-                    <div id="counter" style="text-align: center;     font-family: 'Source Sans Pro',sans-serif;font-weight: bold; font-size: 60px;   display: flex; align-items: center; justify-content: center;"></div>
+                    <div id="counter" style="text-align: left;     font-family: 'Source Sans Pro',sans-serif;font-weight: bold; font-size: 60px;   "></div>
                 </div>
                 <script type="text/javascript">
                 {js_code}
