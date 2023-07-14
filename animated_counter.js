c30adfd19ac61be87b3ed98f268cc8308beff21d
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-14 14:25:22 +0300

    message

--- animated_counter.js
+++ animated_counter.js
@@ -46,20 +46,24 @@ function animateCounter3(elementId, startValue, endValue, duration) {
     }, stepTime);
 }
 
-function animateCounter1_vspan(elementId, duration) {
-    const element = document.getElementById(elementId);
-    const startValue = parseInt(element.getAttribute('data-val'), 10);
-    const endValue = parseInt(element.textContent, 10);
-    let current = startValue;
-    const range = endValue - startValue;
-    const increment = endValue > startValue ? 1 : -1;
-    const stepTime = Math.abs(Math.floor(duration / range));
+function animateCounter_v2span(className, duration) {
+    const elements = document.getElementsByClassName(className);
+  
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      const startValue = parseInt(element.getAttribute('data-val'), 10);
+      const endValue = parseInt(element.textContent, 10);
+      let current = startValue;
+      const range = endValue - startValue;
+      const increment = endValue > startValue ? 1 : -1;
+      const stepTime = Math.abs(Math.floor(duration / range));
     
-    const timer = setInterval(() => {
-      current += increment;
-      element.textContent = current;
-      if (current === endValue) {
-        clearInterval(timer);
-      }
-    }, stepTime);
+      const timer = setInterval(() => {
+        current += increment;
+        element.textContent = current;
+        if (current === endValue) {
+          clearInterval(timer);
+        }
+      }, stepTime);
+    }
   }
--- app.py
+++ app.py
@@ -405,13 +405,13 @@ def ad_button1(id,kpdf,js_code):
 </linearGradient>
 </defs>
 </svg>
-        <span id="counter" class="num" data-val="343">000</span>
+        <span class="num" data-val="343">000</span>
         <span class="text">Σύνολο Εργαζομένων</span>
 		<span class="text2">Γεν. Πληθυσμού</span>
       </div></body>
         <script type="text/javascript">
         {js_code}
-        animateCounter1_vspan("data-val", 2000);  // Increase from 0 to 100 in 1 second
+        animateCounter_v2span("num", 2000);  // Increase from 0 to 100 in 1 second
         </script>""",height=250)
 
 
