6e5caa87d4425023a92435f0fff3835a63be4018
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-14 13:38:33 +0300

    message

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def main():
 
 
 
-    with open( "style.css" ) as css:
+    with open( "style2.css" ) as css:
         st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
     #st.markdown(css_style, unsafe_allow_html=True)
