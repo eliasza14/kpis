7cb9c78f1e5b1e9a38220cef2979fa19654dc69b
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-17 17:56:40 +0300

    message

--- app.py
+++ app.py
@@ -1193,7 +1193,6 @@ def e_button5(id,kpdf,js_code):
                             </div>
                             <div id="counter" style="text-align: left; color:black;font-family:'Source Sans Pro',sans-serif;font-weight: bold; font-size: 45px;"></div>
                             <div>
-                                <div style="text-align:center; color: #8E8D8D; font-size: 12px; font-family:  'Source Sans Pro',sans-serif; font-weight: 300; line-height: 24px; word-wrap: break-word">Yπηρεσίες</div>
                                 <div style="text-align:center; color: #6E7279; font-size: 24px; font-family:  'Source Sans Pro',sans-serif; font-weight: 300; line-height: 24px; word-wrap: break-word">Κτηρίων & Εξ. Χώρων</div>
                             </div>
                         </div>
@@ -1245,7 +1244,6 @@ def e_button5(id,kpdf,js_code):
                             </div>
                             <div id="counter" style="text-align: left; color:black;font-family:'Source Sans Pro',sans-serif;font-weight: bold; font-size: 45px;"></div>
                             <div>
-                                <div style="text-align:center; color: #8E8D8D; font-size: 12px; font-family:  'Source Sans Pro',sans-serif; font-weight: 300; line-height: 24px; word-wrap: break-word">Yπηρεσίες</div>
                                 <div style="text-align:center; color: #6E7279; font-size: 24px; font-family:  'Source Sans Pro',sans-serif; font-weight: 300; line-height: 24px; word-wrap: break-word">Εστίασης</div>
                             </div>
                         </div>
@@ -1296,7 +1294,6 @@ def e_button5(id,kpdf,js_code):
                             </div>
                             <div id="counter" style="text-align: left; color:black;font-family:'Source Sans Pro',sans-serif;font-weight: bold; font-size: 45px;"></div>
                             <div>
-                                <div style="text-align:center; color: #8E8D8D; font-size: 12px; font-family:  'Source Sans Pro',sans-serif; font-weight: 300; line-height: 24px; word-wrap: break-word">Yπηρεσίες</div>
                                 <div style="text-align:center; color: #6E7279; font-size: 24px; font-family:  'Source Sans Pro',sans-serif; font-weight: 300; line-height: 24px; word-wrap: break-word">Λοιπές Δραστηρίοτητες</div>
                             </div>
                         </div>
