896a41ee9302fc02a265aa8072a5298d1f7931ec
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-21 13:20:22 +0300

    message

--- packageCharts.py
+++ packageCharts.py
@@ -292,7 +292,7 @@ def donut_pct_Chart(val,color1,color2,labels):
     hole=0.85,
     textinfo='none',
     marker_colors=[color1, color2],
-    startangle=90
+    startangle=90,
     ))
     fig.update_layout(annotations=[dict(text=str(val) + "%", font_size=40, showarrow=False)])
     fig.update_layout(showlegend=True)  # Show the legend
