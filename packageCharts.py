e9e49798f02bc4ac3f81cbf3d388de6df1807c08
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-21 15:18:28 +0300

    message

--- packageCharts.py
+++ packageCharts.py
@@ -57,11 +57,9 @@ def stackedChart(columns, kpdf, legend_labels, xaxis_title, yaxis_title, colors)
             text=kpdf[col],
             textposition='inside',  # 'inside' places the text at the center of the bars
             marker=dict(color=colors[i]),  # Assign a color from the color palette
-            textfont=dict(size=14, color='white'), xanchor='center', yanchor='middle' # Set the font size and color for the labels
+            textfont=dict(size=14, color='white')  # Set the font size and color for the labels
         ))
-        
- 
-
+    
     # Update the layout
     fig.update_layout(
         barmode='stack',
