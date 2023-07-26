a9a5f3e5d4390d46199074d64baf9e9ec5ce5f6e
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-26 11:55:24 +0300

    message

--- packageKPS.py
+++ packageKPS.py
@@ -87,7 +87,7 @@ def calculate_d15(row):
     if d7 == 0:
         return np.nan
     
-    if d7 == 0 and d13 == 0:
+    elif d7 == 0 and d13 == 0:
         return np.nan
     else:
         return round(float(d13) / int(d7), 1)
