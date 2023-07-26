28b8a566482a03ead28ebcbdc862e9cbee881460
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-26 12:06:26 +0300

    message

--- packageKPS.py
+++ packageKPS.py
@@ -84,10 +84,15 @@ def calculate_d15(row):
     d7 = row['D7']
     d13 = row['D13']
     
-    if d7 != 0 and d13 != 0:
+    try:
         return round(float(d13) / int(d7), 1)
-    else:
+    except ZeroDivisionError:
         return np.nan
+
+    # if d7 != 0 and d13 != 0:
+    #     return round(float(d13) / int(d7), 1)
+    # else:
+    #     return np.nan
     
 
 
