6f103094ecaba6a85cb39023e27ff19772f41dfc
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-07-27 13:03:24 +0300

    message

--- packageCharts.py
+++ packageCharts.py
@@ -91,7 +91,7 @@ def stackedChart(columns, kpdf, legend_labels, xaxis_title, yaxis_title, colors)
         plot_bgcolor='rgba(0,0,0,0)',
         paper_bgcolor='rgba(0,0,0,0)',
         height=600,
-        width=1000
+        width=800
     )
 
     return fig
